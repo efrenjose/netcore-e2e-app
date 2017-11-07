fbd9b98f13ce920231ffb9d2281e65b01091763b
Efren Rodriguez <efrenrodriguez@live.com>
2017-11-06 18:40:22 -0600

    Build initial domain model

---
+++ Models/Make.cs
@@ -0,0 +1,17 @@
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+
+namespace netcore_e2e_app.Models
+{
+    public class Make
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public ICollection<Model> Models { get; set; }
+
+        public Make()
+        {
+            Models = new Collection<Model>();
+        }
+    }
+}
