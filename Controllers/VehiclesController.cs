77deea7c11c1f528197909e9482ad04c61c4d72b
Efren Rodriguez <efrenrodriguez@live.com>
2018-02-26 06:23:06 -0600

    implement api update and collection to collection mapping

--- Controllers/VehiclesController.cs
+++ Controllers/VehiclesController.cs
@@ -2,6 +2,7 @@ using System;
 using System.Threading.Tasks;
 using AutoMapper;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using netcore_e2e_app.Models;
 using netcore_e2e_app.Models.Resources;
 using netcore_e2e_app.Persistence;
@@ -20,16 +21,33 @@ namespace netcore_e2e_app.Controllers
             this.mapper = mapper;
 
         }
+
         [HttpPost]
         public async Task<IActionResult> CreatVehicle([FromBody] VehicleResource vehicleResource)
         {
             if (!ModelState.IsValid)
              return BadRequest(ModelState);
-             
+
             var vehicle = mapper.Map<VehicleResource, Vehicle>(vehicleResource);
             vehicle.LastUpdate = DateTime.Now;
 
             context.Vehicles.Add(vehicle);
+            await context.SaveChangesAsync();
+
+            var result = mapper.Map<Vehicle, VehicleResource>(vehicle);
+            return Ok(result);
+        }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> UpdateVehicle(int id, [FromBody] VehicleResource vehicleResource)
+        {
+            if (!ModelState.IsValid)
+             return BadRequest(ModelState);
+    
+            var vehicle = await context.Vehicles.Include(v => v.Features).SingleOrDefaultAsync(v => v.Id == id);
+            mapper.Map<VehicleResource, Vehicle>(vehicleResource, vehicle);
+            vehicle.LastUpdate = DateTime.Now;
+
             await context.SaveChangesAsync();
 
             var result = mapper.Map<Vehicle, VehicleResource>(vehicle);
